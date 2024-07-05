4fa25dca346a9f3cc0af952febea654e57a224ab
DarkestMuon <154683782+DarkestMuon@users.noreply.github.com>
2024-07-05 10:14:45 +0000

    Create trial.cpp

---
+++ src/trial.cpp
@@ -0,0 +1,12 @@
+#include "renderform/renderform.h"
+
+int main() {
+  Renderform::Formula output = Renderform::parseImage(input_file);
+  // Print to stdout
+  output.print();
+  // Get LaTeX formula
+  std::string latex = output.getLaTeX();
+  // Print LaTeX formula
+  std::cout << latex << std::endl;
+  return 0;
+}
