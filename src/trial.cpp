9b7478294571ef5efae429e92ecedd8c4279bd08
DarkestMuon <154683782+DarkestMuon@users.noreply.github.com>
2024-07-05 18:24:37 +0530

    Update trial.cpp

--- src/trial.cpp
+++ src/trial.cpp
@@ -1,5 +1,4 @@
-#include "../include/renderform/renderform.h"
-
+#include "/content/Renderform/include/renderform/renderform.hpp"
 int main() {
   Renderform::Formula output = Renderform::parseImage(input_file);
   // Print to stdout
