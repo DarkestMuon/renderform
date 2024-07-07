f1ff2ae608642abfd324c0e9aad13115c5ed6857
Abhi <darkestmuon@proton.me>
2024-07-07 13:13:55 +0530

    idk

--- src/trial.cpp
+++ src/trial.cpp
@@ -1,5 +1,6 @@
-#include "/content/Renderform/include/renderform/renderform.hpp"
-int main() {
+#include "renderform.hpp"
+int trial() {
+  std::string input_file = "/home/abhi/Downloads/Screenshot-4210.png";
   Renderform::Formula output = Renderform::parseImage(input_file);
   // Print to stdout
   output.print();
